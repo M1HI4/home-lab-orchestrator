cd2f1612cf14b82720cb628fcb0fd71eaf8c3e54
M1HI4 <michael.hanshu@yandex.ru>
2026-06-18 23:24:31 +0300

    Настройка Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+terraform {
+	required_providers {
+		local = {
+		 source = "hashicorp/local"
+		 version = "~> 2.1"
+		}
+	}
+}
+resource "local_file" "example" {
+	content = var.message
+	filename = "${path.module}/output.txt"
+}
